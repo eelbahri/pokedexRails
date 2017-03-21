840bd4e2fc6dc9cffbc696214c1dc346f3f7d0c2
eelbahri <elyes.elbahri77@gmail.com>
2017-03-21 17:13:04 +0100

    complete crud

--- README.md
+++ README.md
@@ -1,36 +1,13 @@
-# README
+# Cours Rails
 
 # Cours
 
+###### Remember
+
+ - rails s
+
 ### 21/03/17
+
 > Ajout de bootstrap-sass
 > Ajout de meta dans les vues
 > CRUD
-
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-rails s
-
-
-
-* ...
-# cours rails
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,14 +1,52 @@
 class PokemonsController < ApplicationController
+    before_action :set_pokemon, only: [:show, :edit, :update, :destroy] # boje
+
     def index
         # Le @ envoie la variable à la vue
         @pokemons = Pokemon.all
     end
 
     def show
-        @pokemon = Pokemon.find(params[:id])
     end
 
     def new
         @pokemon = Pokemon.new
+    end
+
+    def create
+        @pokemon = Pokemon.new pokemon_params
+        # permit est obligatoire pour la sécurité, il spécifie les champs que l'on veut
+        if @pokemon.save
+            # l'action show est reconnu par rails pour l'affichage d'objet
+            redirect_to @pokemon
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @pokemon.update pokemon_params
+            redirect_to @pokemon
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @pokemon.destroy
+        redirect_to pokemons_path
+    end
+
+    private # pour factoriser certaines lignes
+
+    def set_pokemon
+        @pokemon = Pokemon.find(params[:id])
+    end
+
+    def pokemon_params
+        params.require(:pokemon).permit(:name)
     end
 end
